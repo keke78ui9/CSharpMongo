79f2cb46275c2c2f7db00fd24f1cc6376c6c00dc
keke78ui9 <keke78ui9@gmail.com>
2015-10-11 20:20:51 -0700

    adding missing class

---
+++ CSharpMongo/IMongo.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq.Expressions;
+
+namespace CSharpMongo
+{
+    public interface IMongo
+    {
+        MongoClient Client { get; }
+        IMongoDatabase Database { get; }
+        MongoUrl Url { get; }
+    }
+}
